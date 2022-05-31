b49b7b6595f462b032f4968d8d26ff9774d3ce5f
Marian <marianlopig@gmail.com>
2022-05-31 23:08:18 +0200

    changes in handlers for url test

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,15 +1,12 @@
 import { rest } from "msw";
 
 export const usersHandlers = [
-  rest.post(
-    "https://marian-lopez-back-final-project-202204.onrender.com/users/login",
-    (req, res, ctx) => {
-      return res(
-        ctx.status(200),
-        ctx.json({
-          token: "mocktoken",
-        })
-      );
-    }
-  ),
+  rest.post(`${process.env.REACT_APP_API_URL}/users/login`, (req, res, ctx) => {
+    return res(
+      ctx.status(200),
+      ctx.json({
+        token: "mocktoken",
+      })
+    );
+  }),
 ];
