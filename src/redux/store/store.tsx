ad9a5f5e5f0490ca6f0391324687e117e681d758
Marian <marianlopig@gmail.com>
2022-05-31 11:24:53 +0200

    main redux elements, store created, hook and userSlice

---
+++ src/redux/store/store.tsx
@@ -0,0 +1,9 @@
+import { configureStore } from "@reduxjs/toolkit";
+import usersReducer from "../userSlice/userSlice";
+
+const store = configureStore({
+  reducer: { users: usersReducer },
+});
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
