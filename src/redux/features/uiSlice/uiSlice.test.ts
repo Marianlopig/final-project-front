0fbc57b80e979485bb7e03b42e15c38bf3824674
Marian <marianlopig@gmail.com>
2022-06-01 22:40:58 +0200

    test for notloading slice

--- src/redux/features/uiSlice/uiSlice.test.ts
+++ src/redux/features/uiSlice/uiSlice.test.ts
@@ -1,4 +1,7 @@
-import uiReducer, { loadingActionCreator } from "./uiSlice";
+import uiReducer, {
+  loadingActionCreator,
+  notLoadingActionCreator,
+} from "./uiSlice";
 
 describe("Given a uiSlice with an loadingActionCreator", () => {
   describe("When we dispatch the loading action", () => {
@@ -7,6 +10,20 @@ describe("Given a uiSlice with an loadingActionCreator", () => {
       const expected = { showSpinner: true };
 
       const action = loadingActionCreator();
+      const newState = uiReducer(initialState, action);
+
+      expect(newState).toEqual(expected);
+    });
+  });
+});
+
+describe("Given a uiSlice with an notloadingActionCreator", () => {
+  describe("When we dispatch the notloading action", () => {
+    test("Then it should return the new state with show spinner false", () => {
+      const initialState = { showSpinner: true };
+      const expected = { showSpinner: false };
+
+      const action = notLoadingActionCreator();
       const newState = uiReducer(initialState, action);
 
       expect(newState).toEqual(expected);
