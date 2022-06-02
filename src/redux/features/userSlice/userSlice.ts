c8e04bc8da7aac30e31c77d1de8729cc79b4471e
Marian <marianlopig@gmail.com>
2022-06-03 00:52:24 +0200

    deleted unused import from userSlice

--- src/redux/features/userSlice/userSlice.ts
+++ src/redux/features/userSlice/userSlice.ts
@@ -1,5 +1,4 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
-import type { RootState } from "../../store/store";
 import { LoginResponse } from "../../types/userInterfaces";
 
 const initialState: LoginResponse = {
