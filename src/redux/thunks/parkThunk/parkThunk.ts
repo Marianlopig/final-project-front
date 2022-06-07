ae4d5476ede05cc57737c765aa0014227708348a
Marian <marianlopig@gmail.com>
2022-06-07 23:41:31 +0200

    thunk for create parks

--- src/redux/thunks/parkThunk/parkThunk.ts
+++ src/redux/thunks/parkThunk/parkThunk.ts
@@ -1,11 +1,16 @@
 import { Dispatch } from "@reduxjs/toolkit";
 import axios from "axios";
-import { deleteParkActionCreator } from "../../features/accountSlice/accountSlice";
+import {
+  addParkActionCreator,
+  deleteParkActionCreator,
+} from "../../features/accountSlice/accountSlice";
 import { loadParksActionCreator } from "../../features/parksSlice/parkSlice";
 import {
   loadingActionCreator,
   notLoadingActionCreator,
 } from "../../features/uiSlice/uiSlice";
+import { IPark } from "../../types/parkInterfaces";
+import { toast } from "react-toastify";
 
 const url: string = `${process.env.REACT_APP_API_URL}/parks`;
 
@@ -18,11 +23,14 @@ const getAuth = () => {
 };
 
 export const loadParksThunk = () => async (dispatch: Dispatch) => {
-  const { data, status } = await axios.get(`${url}/list`);
-  dispatch(loadingActionCreator());
+  try {
+    const { data, status } = await axios.get(`${url}/list`);
+    dispatch(loadingActionCreator());
 
-  if (status === 200) {
-    dispatch(loadParksActionCreator(data));
+    if (status === 200) {
+      dispatch(loadParksActionCreator(data));
+    }
+  } finally {
     dispatch(notLoadingActionCreator());
   }
 };
@@ -31,5 +39,20 @@ export const deleteParkThunk = (id: string) => async (dispatch: Dispatch) => {
   const { status } = await axios.delete(`${url}/${id}`, getAuth());
   if (status === 200) {
     dispatch(deleteParkActionCreator(id));
+  }
+};
+
+export const createParkThunk = (park: IPark) => async (dispatch: Dispatch) => {
+  try {
+    dispatch(loadingActionCreator());
+    const { status, data } = await axios.post(`${url}/`, park, getAuth());
+    if (status === 201) {
+      toast.success("Park created! Thanks!");
+      dispatch(addParkActionCreator(data));
+    } else {
+      toast.error(`Error creating the park`);
+    }
+  } finally {
+    dispatch(notLoadingActionCreator());
   }
 };
