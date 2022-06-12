a3b8eb64d1552db27e538be024afba4610df1765
Marian <marianlopig@gmail.com>
2022-06-12 21:46:40 +0200

    add and remove park from favourites

---
+++ src/redux/thunks/accountThunk/accountThunk.ts
@@ -0,0 +1,69 @@
+import { Dispatch } from "@reduxjs/toolkit";
+import axios from "axios";
+import { toast } from "react-toastify";
+import {
+  addFavouriteActionCreator,
+  deleteFavouriteActionCreator,
+} from "../../features/accountSlice/accountSlice";
+import {
+  loadingActionCreator,
+  notLoadingActionCreator,
+} from "../../features/uiSlice/uiSlice";
+
+const url: string = `${process.env.REACT_APP_API_URL}/users`;
+const getAuth = () => {
+  const token = localStorage.getItem("token");
+  return {
+    headers: { Authorization: `Bearer ${token}` },
+  };
+};
+
+export const addFavouriteThunk =
+  (parkId: string) => async (dispatch: Dispatch) => {
+    try {
+      dispatch(loadingActionCreator());
+      const { status } = await axios.put(
+        `${url}/addfavourite`,
+        {
+          id: parkId,
+        },
+        getAuth()
+      );
+      dispatch(addFavouriteActionCreator(parkId));
+
+      if (status === 200) {
+        toast.success("Park added to favourites");
+      } else {
+        toast.error(`Error adding the park to favourites, try again!`);
+      }
+    } catch (e) {
+      toast.error(`Error adding the park to favourites, try again!`);
+    } finally {
+      dispatch(notLoadingActionCreator());
+    }
+  };
+
+export const deleteFavouriteThunk =
+  (parkId: string) => async (dispatch: Dispatch) => {
+    try {
+      dispatch(loadingActionCreator());
+      const { status } = await axios.put(
+        `${url}/deletefavourite`,
+        {
+          id: parkId,
+        },
+        getAuth()
+      );
+      dispatch(deleteFavouriteActionCreator(parkId));
+
+      if (status === 200) {
+        toast.success("Park deleted from favourites");
+      } else {
+        toast.error(`Error deleting park from favourites, try again!`);
+      }
+    } catch (e) {
+      toast.error(`Error deleting park from favourites, try again!`);
+    } finally {
+      dispatch(notLoadingActionCreator());
+    }
+  };
