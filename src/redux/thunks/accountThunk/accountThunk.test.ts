331badf74776ddc0cb369f31ec932891031244a5
Marian <marianlopig@gmail.com>
2022-06-15 22:43:34 +0200

    tested delete from favourite thunk

--- src/redux/thunks/accountThunk/accountThunk.test.ts
+++ src/redux/thunks/accountThunk/accountThunk.test.ts
@@ -3,11 +3,7 @@ import {
   addFavouriteActionCreator,
   deleteFavouriteActionCreator,
 } from "../../features/accountSlice/accountSlice";
-import {
-  addFavouriteThunk,
-  deleteFavouriteThunk,
-  loadAccountThunk,
-} from "./accountThunk";
+import { addFavouriteThunk, deleteFavouriteThunk } from "./accountThunk";
 import { toast } from "react-toastify";
 import { server } from "../../../mocks/server";
 import { rest } from "msw";
@@ -31,7 +27,7 @@ describe("Given an addFavouriteThunk function", () => {
   });
 
   describe("When it has an error", () => {
-    test.only("Then it should throw a toast with an error 'Error adding the park to favoruites'", async () => {
+    test("Then it should throw a toast with an error 'Error adding the park to favoruites'", async () => {
       toast.error = jest.fn();
 
       server.use(
@@ -65,6 +61,27 @@ describe("Given an deleteFavouriteThunk function", () => {
       await thunk(dispatch);
 
       expect(dispatch).toHaveBeenCalledWith(expectedAction);
+    });
+  });
+
+  describe("When it has an error", () => {
+    test.only("Then it should throw a toast with an error 'Error deleting the park from favoruites'", async () => {
+      toast.error = jest.fn();
+
+      server.use(
+        rest.put(
+          `${process.env.REACT_APP_API_URL}/users/deletefavourite`,
+          (req, res, ctx) => {
+            return res(ctx.status(400), ctx.json({}));
+          }
+        )
+      );
+
+      const idMockPark = "629f8aec8c2b3037ff6aeb4d";
+
+      await deleteFavouriteThunk(idMockPark)(jest.fn());
+
+      expect(toast.error).toHaveBeenCalled();
     });
   });
 });
