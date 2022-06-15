2956b1cb7eb3a113c5f123bc7fc3e818573663c8
Marian <marianlopig@gmail.com>
2022-06-15 16:37:16 +0200

    not commented data inside the document

--- src/redux/thunks/authThunk/authThunkotracosa.ts
+++ src/redux/thunks/authThunk/authThunkotracosa.ts
@@ -1,58 +1,58 @@
-// import "../../../mocks/server";
-// import { loginActionCreator } from "../../features/authSlice/authSlice";
-// import { loginThunk } from "./authThunk";
+import "../../../mocks/server";
+import { loginActionCreator } from "../../features/authSlice/authSlice";
+import { loginThunk } from "./authThunk";
 
-// jest.mock("jwt-decode", () => () => ({
-//   username: "marian",
-//   name: "marian",
-//   userId: "1",
-// }));
+jest.mock("jwt-decode", () => () => ({
+  username: "marian",
+  name: "marian",
+  userId: "1",
+}));
 
-// describe("Given a loginThunk function", () => {
-//   describe("When it is called", () => {
-//     test("It should dispatch loginActionCreator with the data from the token", async () => {
-//       const dispatch = jest.fn();
+describe("Given a loginThunk function", () => {
+  describe("When it is called", () => {
+    test("It should dispatch loginActionCreator with the data from the token", async () => {
+      const dispatch = jest.fn();
 
-//       const expectedData = {
-//         username: "marian",
-//         name: "marian",
-//         userId: "1",
-//       };
-//       const userData = {
-//         username: "marian",
-//         password: "password",
-//         name: "marian",
-//         email: "test",
-//         city: "Barcelona",
-//       };
+      const expectedData = {
+        username: "marian",
+        name: "marian",
+        userId: "1",
+      };
+      const userData = {
+        username: "marian",
+        password: "password",
+        name: "marian",
+        email: "test",
+        city: "Barcelona",
+      };
 
-//       const expectedAction = loginActionCreator(expectedData);
+      const expectedAction = loginActionCreator(expectedData);
 
-//       const thunk = loginThunk(userData);
-//       await thunk(dispatch);
+      const thunk = loginThunk(userData);
+      await thunk(dispatch);
 
-//       expect(dispatch).toHaveBeenCalledWith(expectedAction);
-//     });
-//   });
-// });
+      expect(dispatch).toHaveBeenCalledWith(expectedAction);
+    });
+  });
+});
 
-// describe("Given a RegisterThunk function", () => {
-//   describe("When it is called", () => {
-//     test("It should dispatch the register thunk with the user data", async () => {
-//       const dispatch = jest.fn();
+describe("Given a RegisterThunk function", () => {
+  describe("When it is called", () => {
+    test("It should dispatch the register thunk with the user data", async () => {
+      const dispatch = jest.fn();
 
-//       const userData = {
-//         username: "marian",
-//         password: "password",
-//         name: "marian",
-//         email: "test",
-//         city: "Barcelona",
-//       };
+      const userData = {
+        username: "marian",
+        password: "password",
+        name: "marian",
+        email: "test",
+        city: "Barcelona",
+      };
 
-//       const thunk = loginThunk(userData);
-//       await thunk(dispatch);
+      const thunk = loginThunk(userData);
+      await thunk(dispatch);
 
-//       expect(dispatch).toHaveBeenCalled();
-//     });
-//   });
-// });
+      expect(dispatch).toHaveBeenCalled();
+    });
+  });
+});
